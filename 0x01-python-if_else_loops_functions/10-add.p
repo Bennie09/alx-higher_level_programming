9d2e0319c3126d85fcaf080666bfab7100acd75f
Bennie09 <135521993+Bennie09@users.noreply.github.com>
2024-01-19 21:35:05 +0100

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/10-add.p
+++
@@ -1,6 +0,0 @@
-dd.py
-
-
-def add(a, b):
-    """Return the addition of a and b."""
-    return (a + b)
--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++
@@ -1,8 +0,0 @@
-har_at.py
-
-
-def remove_char_at(str, n):
-        """Create a copy of the string without the character at position n."""
-            if n < 0:
-                        return (str)
-                        return (str[:n] + str[n+1:])
